48ca2d8824ca422cffc79ab0f56733d5dd3d8169
malikjawad34 <jawadiqbl3344@gmail.com>
2024-03-24 19:00:45 +0500

    styles added

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,5 @@
-
 import React from 'react';
-import { Drawer, List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
+import { Drawer, List, ListItem, ListItemIcon, ListItemText, Typography } from '@mui/material';
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import PeopleIcon from '@mui/icons-material/People';
 import { Link } from 'react-router-dom';
@@ -11,24 +10,56 @@ const Sidebar = () => {
   return (
     <Drawer
       variant="permanent"
-      style={{ width: drawerWidth, flexShrink: 0 }}
-      classes={{ paper: drawerWidth }}
+      sx={{
+        width: drawerWidth,
+        flexShrink: 0,
+        '& .MuiDrawer-paper': {
+          width: drawerWidth,
+          boxSizing: 'border-box',
+          backgroundColor: '#2C3E50', // Customize the background color
+          color: 'white', // Text color
+        },
+      }}
     >
+      <Typography
+        variant="h6"
+        sx={{
+          padding: 2,
+          color: 'lightgray', // Title color
+        }}
+      >
+        
+      </Typography>
       <List>
-        <ListItem component={Link} to="/">
-          <ListItemIcon>
+        <ListItem
+          component={Link}
+          to="/"
+          sx={{
+            '&:hover': {
+              backgroundColor: '#34495E', // Darken on hover
+            },
+          }}
+        >
+          <ListItemIcon sx={{ color: 'inherit' }}>
             <DashboardIcon />
           </ListItemIcon>
-          <ListItemText primary="Dashboard" />
+          <ListItemText primary="Dashboard" sx={{ color: 'lightgray' }}/>
         </ListItem>
-      </List>
-      <List>
-        <ListItem component={Link} to="/signup">
-          <ListItemIcon>
+        <ListItem
+          component={Link}
+          to="/signup"
+          sx={{
+            '&:hover': {
+              backgroundColor: '#34495E', // Darken on hover
+            },
+          }}
+        >
+          <ListItemIcon sx={{ color: 'inherit' }}>
             <PeopleIcon />
           </ListItemIcon>
-          <ListItemText primary="Sign Up" />
+          <ListItemText primary="Sign Up" sx={{ color: 'lightgray' }}/>
         </ListItem>
+        {/* Add more list items here as needed */}
       </List>
     </Drawer>
   );
